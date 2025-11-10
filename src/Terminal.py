6a5da73ecb7f06b46b4e3c606894194ca87c3c38
Matthew-Papesh <papesh.m2@gmail.com>
2025-11-10 01:28:43 -0500

    test4

--- src/Terminal.py
+++ src/Terminal.py
@@ -75,7 +75,7 @@ def run_bash_cmd(cmd: list, cwd:path=None):
         # print commands printed from the current working directory 
         margin = " "*4 # margin for offset
         # get name from cwd directory and pop off the end     
-        repo_name = cwd.__str__().split(slash()).pop()
+        repo_name = cwd.__str__().split(slash()).pop() 
         cmd_str = " ".join(result.args) # get list of args 
         # format cwd and command line args to show the bash command
         print(f"{margin}{Terminal.Text.BOLD}{Terminal.Text.YELLOW}{cwd}{slash()}{Terminal.Text.BLUE}{repo_name}>{Terminal.Text.RESET} {cmd_str}")
