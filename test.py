48cc0ec212b4f58ea3e23b7e1e7333e1b16ca604
Matthew-Papesh <papesh.m2@gmail.com>
2025-11-20 12:51:54 -0500

    test

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ from pathlib import Path as path
 
 result = subprocess.run(["git", "push"], cwd= path.home() / "Documents" / "GitHub" / "GitCAD", check=False, capture_output=True, shell=True)
 
-print(result)
+print(result) 
