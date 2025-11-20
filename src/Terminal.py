2e1c6204b31ae760ffb005de04ff522351ea5114
Matthew-Papesh <papesh.m2@gmail.com>
2025-11-20 14:49:30 -0500

    test5

--- src/Terminal.py
+++ src/Terminal.py
@@ -74,9 +74,7 @@ def run_bash_cmd(cmd: list, cwd:path=None):
         shell = sys.platform.startswith("win") # only use shell on windows
         cmd_env = os.environ.copy() 
         
-        
-        cmd_env["GIT_SSH_COMMAND"] = "ssh -o BatchMode=yes"
-        result = subprocess.run(cmd, cwd=cwd, check=False, capture_output=True, shell=shell, env=cmd_env)
+        result = subprocess.run(cmd, cwd=cwd, check=False, capture_output=True, shell=shell, env=cmd_env,stdin=subprocess.DEVNULL)
         # print commands printed from the current working directory 
         margin = " "*4 # margin for offset 
         # get name from cwd directory and pop off the end     
