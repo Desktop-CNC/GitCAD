d8dc961a10aad0a7f382e4e4ddbc9b16c435bb13
Matthew-Papesh <papesh.m2@gmail.com>
2025-11-20 14:35:16 -0500

    added ssh sock print

--- src/Terminal.py
+++ src/Terminal.py
@@ -73,8 +73,8 @@ def run_bash_cmd(cmd: list, cwd:path=None):
     try:
         shell = sys.platform.startswith("win") # only use shell on windows
         cmd_env = os.environ.copy() 
-        #cmd_env["GIT_SSH_COMMAND"] = "ssh -o BatchMode=yes"
-        result = subprocess.run(cmd, cwd=cwd, check=False, capture_output=True, shell=shell, env=cmd_env, stdin=subprocess.DEVNULL)
+        print(f"sock: {cmd_env.get('SSH_AUTH_SOCK')}")
+        result = subprocess.run(cmd, cwd=cwd, check=False, capture_output=True, shell=shell, env=cmd_env)
         # print commands printed from the current working directory 
         margin = " "*4 # margin for offset 
         # get name from cwd directory and pop off the end     
